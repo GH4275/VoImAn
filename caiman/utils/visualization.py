GH4275/VoImAn

commit a53a4efc3ba9b3f9947b8365419ea465fac70171

maintaining aspect ratio when plotting in notebook

caiman/utils/visualization.py
@@ -230,7 +230,9 @@ def nb_view_patches(Yr, A, C, b, f, d1, d2, YrA=None, image_neurons=None, thr=0.
                                  title="Neuron Number", callback=callback)
     xr = Range1d(start=0, end=image_neurons.shape[1])
     yr = Range1d(start=image_neurons.shape[0], end=0)
-    plot1 = bpl.figure(x_range=xr, y_range=yr, plot_width=300, plot_height=300)
+    plot1 = bpl.figure(x_range=xr, y_range=yr,
+                       plot_width=int(min(1, d2/d1))*300,
+                       plot_height=int(min(1, d1/d2))*300)
 
     plot1.image(image=[image_neurons[::-1, :]], x=0,
                 y=image_neurons.shape[0], dw=d2, dh=d1, palette=grayp)

commit d858769e0a546e01afbf5e0de785ba719f8a676a

fix plotting bug introduced with a53a4efc3ba9b3f9947b8365419ea465fac70171

caiman/utils/visualization.py
@@ -232,8 +232,8 @@ def nb_view_patches(Yr, A, C, b, f, d1, d2, YrA=None, image_neurons=None, thr=0.
     xr = Range1d(start=0, end=image_neurons.shape[1])
     yr = Range1d(start=image_neurons.shape[0], end=0)
     plot1 = bpl.figure(x_range=xr, y_range=yr,
-                       plot_width=int(min(1, d2/d1))*300,
-                       plot_height=int(min(1, d1/d2))*300)
+                       plot_width=int(min(1, d2/d1)*300),
+                       plot_height=int(min(1, d1/d2)*300))
 
     plot1.image(image=[image_neurons[::-1, :]], x=0,
                 y=image_neurons.shape[0], dw=d2, dh=d1, palette=grayp)