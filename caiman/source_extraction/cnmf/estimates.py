GH4275/VoImAn

commit 48b644d30be4d063619511a160e8e422deeac0eb

support for NWB and GUI

caiman/source_extraction/cnmf/estimates.py
@@ -849,7 +849,7 @@ class Estimates(object):
 
         return self
 
-    def filter_components(self, imgs, params, new_dict={}, dview=None):
+    def filter_components(self, imgs, params, new_dict={}, dview=None, select_mode='All'):
         """Filters components based on given thresholds without re-computing
         the quality metrics. If the quality metrics are not present then it
         calls self.evaluate components.
@@ -860,6 +860,12 @@ class Estimates(object):
 
             params: params object
                 Parameters of the algorithm
+
+            select_mode: str
+                Can be 'All' (no subselection is made, but quality filtering is performed),
+                'Accepted' (subselection of accepted components, a field named self.accepted_list must exist),
+                'Rejected' (subselection of rejected components, a field named self.rejected_list must exist),
+                'Unassigned' (both fields above need to exist)
 
             new_dict: dict
                 New dictionary with parameters to be called. The dictionary
@@ -923,6 +929,15 @@ class Estimates(object):
                                            thresh_cnn_lowest=opts['cnn_lowest'],
                                            use_cnn=opts['use_cnn'],
                                            gSig_range=opts['gSig_range'])
+
+        if select_mode == 'Accepted':
+           self.idx_components = np.array(np.intersect1d(self.idx_components,self.accepted_list))
+        elif select_mode == 'Rejected':
+           self.idx_components = np.array(np.intersect1d(self.idx_components,self.rejected_list))
+        elif select_mode == 'Unassigned':
+           self.idx_components = np.array(np.setdiff1d(self.idx_components,np.union1d(self.rejected_list,self.accepted_list)))
+
+        self.idx_components_bad = np.array(np.setdiff1d(range(len(self.SNR_comp)),self.idx_components))
 
         return self
 
@@ -1140,13 +1155,9 @@ class Estimates(object):
         else:
             components_to_keep = np.arange(self.A.shape[-1])
 
-        if self.idx_components is None:
-            self.idx_components = np.arange(self.A.shape[-1])
-        self.idx_components = np.intersect1d(self.idx_components, components_to_keep)
-        self.idx_components_bad = np.setdiff1d(np.arange(self.A.shape[-1]), self.idx_components)
-        #self.select_components(idx_components=components_to_keep)
-        if select_comp:
-            self.select_components(use_object=True)
+
+
+        self.select_components(idx_components=components_to_keep)
 
         return components_to_keep
 
@@ -1156,6 +1167,104 @@ class Estimates(object):
                 'You need to compute thresolded components before calling this method: use the threshold_components method')
         bin_masks = self.A_thr.reshape([self.dims[0], self.dims[1], -1], order='F').transpose([2, 0, 1])
         return nf_masks_to_neurof_dict(bin_masks, dataset_name)
+
+    def save(self,
+             filename,
+             imaging_plane_name = None,
+             imaging_series_name = None,
+             sess_desc='CaImAn Results',
+             exp_desc=None,
+             imaging_rate=30,
+             location='somewhere in the brain',
+             orig_file_format='tiff'):
+        """save object in hdf5 file format
+
+        Args:
+            filename: str
+                path to the hdf5 file containing the saved object
+        """
+    #%%
+        from pynwb import NWBHDF5IO
+        from pynwb.ophys import ImageSegmentation, Fluorescence,MotionCorrection
+        import os
+
+    #%%
+        if '.nwb' != filename[-4:]:
+            raise Exception("Wrong filename")
+
+        if not os.path.isfile(filename): # if the file doesn't exist create new and add the orginal data path
+            raise Exception('filename should be an existing NWB file. \
+                            Consdier using cnmf.movie.save method to create one.')
+
+        else: # if the file already exist in the .nwb format then just add the results to it
+            print('Saving the results...')
+            import pdb
+            pdb.set_trace()
+            with  NWBHDF5IO(filename, 'r+') as io:
+                nwbfile = io.read()
+                # Add processing results
+                mod = nwbfile.create_processing_module('Estimates', 'contains caiman estimates for the main imagin plane')
+                img_seg = ImageSegmentation()
+                mod.add_data_interface(img_seg)
+                fl = Fluorescence()
+                mod.add_data_interface(fl)
+    #            mot_crct = MotionCorrection()
+    #            mod.add_data_interface(mot_crct)
+
+                # Add the ROI-related stuff
+                if imaging_plane_name is None:
+                    imaging_plane_name = [imp for imp in nwbfile.imaging_planes.keys()]
+                    if len(imaging_plane_name)>1:
+                        raise Exception('There is more than one imaging plane in the file, you need to specify the name via '
+                                        'the "imaging_plane_name" parameter')
+                    else:
+                        imaging_plane_name = imaging_plane_name[0]
+
+                if imaging_series_name is None:
+                    imaging_series_name = [imp for imp in nwbfile.acquisition.keys()]
+                    if len(imaging_series_name)>1:
+                        raise Exception('There is more than one imaging plane in the file, you need to specify the name via '
+                                        'the "imaging_series_name" parameter')
+                    else:
+                        imaging_series_name = imaging_series_name[0]
+
+
+                imaging_plane = nwbfile.imaging_planes[imaging_plane_name]
+                image_series = nwbfile.acquisition[imaging_series_name]
+
+                ps = img_seg.create_plane_segmentation('CNMF_ROIs',
+                                                       imaging_plane, 'planeseg', image_series)
+
+                # Add ROIs
+                # Neurons
+                for roi in self.A.T:
+                    ps.add_roi(image_mask=roi.T.toarray().reshape(self.dims))
+                # Backgrounds
+                for bg in self.b.T:
+                    ps.add_roi(image_mask=bg.reshape(self.dims))
+
+                # Add Traces
+                n_rois = self.A.shape[-1]
+                n_bg = len(self.f)
+                rt_region_roi = ps.create_roi_table_region('ROIs',
+                                                       region=list(range(n_rois)))
+
+                rt_region_bg = ps.create_roi_table_region('Background',
+                                                       region=list(range(n_rois,n_rois+n_bg)))
+
+                timestamps = list(range(self.f.shape[1]))
+
+                # Neurons
+                rrs1 = fl.create_roi_response_series('ROI_Fluorescence_Response', self.C.T, 'lumens', rt_region_roi, timestamps=timestamps)
+                # Background
+                rrs2 = fl.create_roi_response_series('Background_Fluorescence_Response', self.f.T, 'lumens', rt_region_bg, timestamps=timestamps)
+
+                # Add MotionCorreciton
+    #            create_corrected_image_stack(corrected, original, xy_translation, name='CorrectedImageStack')
+
+                io.write(nwbfile)
+
+
 
 def compare_components(estimate_gt, estimate_cmp,  Cn=None, thresh_cost=.8, min_dist=10, print_assignment=False, labels=['GT', 'CMP'], plot_results=False):
     if estimate_gt.A_thr is None:

caiman/base/movies.py
@@ -1328,16 +1328,22 @@ def load(file_name, fr:float=30, start_time:float=0, meta_data:Dict=None, subind
                     fkeys = list(f.keys())
                     if len(fkeys) == 1:
                         var_name_hdf5 = fkeys[0]
+
+                    if extension == '.nwb':
+                        fgroup = f[var_name_hdf5]['data']
+                    else:
+                        fgroup = f[var_name_hdf5]
+
                     if var_name_hdf5 in f:
                         if subindices is None:
-                            images = np.array(f[var_name_hdf5]).squeeze()
+                            images = np.array(fgroup).squeeze()
                             #if images.ndim > 3:
                             #    images = images[:, 0]
                         else:
                             if type(subindices).__module__ is 'numpy':
                                 subindices = subindices.tolist()
                             images = np.array(
-                                f[var_name_hdf5][subindices]).squeeze()
+                                fgroup[subindices]).squeeze()
                             #if images.ndim > 3:
                             #    images = images[:, 0]
 
@@ -1400,7 +1406,7 @@ def load(file_name, fr:float=30, start_time:float=0, meta_data:Dict=None, subind
 def load_movie_chain(file_list:List[str], fr:float=30, start_time=0,
                      meta_data=None, subindices=None,
                      bottom=0, top=0, left=0, right=0, z_top=0,
-                     z_bottom=0, is3D:bool=False, channel=None, outtype=np.float32) -> Any:
+                     z_bottom=0, is3D:bool=False, channel=None, outtype=np.float32, var_name_hdf5:str='mov') -> Any:
     """ load movies from list of file names
 
     Args:
@@ -1423,7 +1429,7 @@ def load_movie_chain(file_list:List[str], fr:float=30, start_time=0,
     mov = []
     for f in tqdm(file_list):
         m = load(f, fr=fr, start_time=start_time,
-                 meta_data=meta_data, subindices=subindices, in_memory=True, outtype=outtype)
+                 meta_data=meta_data, subindices=subindices, in_memory=True, outtype=outtype, var_name_hdf5=var_name_hdf5)
         if channel is not None:
             logging.debug(m.shape)
             m = m[channel].squeeze()

caiman/base/timeseries.py
@@ -18,6 +18,9 @@ import pickle as cpk
 from scipy.io import savemat
 import tifffile
 import warnings
+from datetime import datetime
+from dateutil.tz import tzlocal
+
 
 from caiman.paths import memmap_frames_filename
 
@@ -114,8 +117,28 @@ class timeseries(np.ndarray):
         self.file_name = getattr(obj, 'file_name', None)
         self.meta_data = getattr(obj, 'meta_data', None)
 
-    def save(self, file_name, to32=True, order='F',imagej=False, bigtiff=True,
-             software='CaImAn', compress=0, var_name_hdf5='mov'):
+    def save(self,
+             file_name,
+             to32=True,
+             order='F',
+             imagej=False,
+             bigtiff=True,
+             excitation_lambda=488.0,
+             compress=0,
+             var_name_hdf5='mov',
+             sess_desc='some_description',
+             identifier = 'some identifier',
+             exp_desc = 'experiment description',
+             imaging_plane_description = 'spome imaging plane description',
+             emission_lambda = 520.0,
+             indicator = 'OGB-1',
+             location = 'brain',
+             starting_time = 0.,
+             experimenter='Dr Who',
+             lab_name='NEL',
+             institution='UNC-CH',
+             experiment_description='Experiment Description',
+             session_id='Session ID'):
         """
         Save the timeseries in various formats
 
@@ -245,6 +268,64 @@ class timeseries(np.ndarray):
             big_mov.flush()
             del big_mov, input_arr
             return fname_tot
+
+
+
+        elif extension == '.nwb':
+            from pynwb import NWBHDF5IO
+            from pynwb import NWBFile
+            from pynwb.ophys import TwoPhotonSeries, OpticalChannel, ImageSegmentation, Fluorescence, MotionCorrection
+            from pynwb.device import Device
+            if to32 and not('float32' in str(self.dtype)):
+                input_arr = self.astype(np.float32)
+            else:
+                input_arr = np.array(self)
+            # Create NWB file
+            nwbfile = NWBFile(sess_desc, identifier,
+                              datetime.now(tzlocal()),
+                              experimenter=experimenter,
+                              lab=lab_name,
+                              institution=institution,
+                              experiment_description=experiment_description,
+                              session_id=session_id)
+            # Get the device
+            device = Device('imaging_device')
+            nwbfile.add_device(device)
+            # OpticalChannel
+            optical_channel = OpticalChannel('main_opt_channel',
+                                             'main optical channel',
+                                             emission_lambda = emission_lambda)
+            imaging_plane = nwbfile.create_imaging_plane(name = 'main_imaging_plane',
+                                                optical_channel = optical_channel,
+                                                description = imaging_plane_description,
+                                                device = device,
+                                                excitation_lambda = excitation_lambda,
+                                                imaging_rate = self.fr,
+                                                indicator = indicator,
+                                                location = location)
+            # Images
+            image_series = TwoPhotonSeries(name='mov', dimension=self.shape[1:],
+                                   data = input_arr,
+                                   imaging_plane=imaging_plane,
+                                   starting_frame=[0],
+                                   starting_time=starting_time,
+                                   rate=self.fr)
+
+            nwbfile.add_acquisition(image_series)
+
+            with NWBHDF5IO(file_name, 'w') as io:
+                io.write(nwbfile)
+
+            return file_name
+
+
+
+
+
+
+
+
+
 
         else:
             logging.error("Extension " + str(extension) + " unknown")

caiman/gui/gui.py
@@ -1,270 +1,0 @@
-#!/usr/bin/env python
-
-#%%
-from builtins import zip
-import numpy as np
-import matplotlib.pyplot as pl
-from matplotlib import animation
-from matplotlib.widgets import Slider, Button, RadioButtons
-import matplotlib.gridspec as gridspec
-import time
-
-
-def pretty_roi(mask):
-    mask = mask.astype(np.float)
-    colors = np.arange(1, len(mask) + 1)
-    mask *= colors[..., np.newaxis, np.newaxis]
-    mask = np.max(mask, axis=0)
-    mask[mask == 0] = None
-    return mask
-
-
-def isin(r, ax):
-    x0, x1, y0, y1 = ax.axis()
-    xmin, xmax = min(x0, x1), max(x0, x1)
-    ymin, ymax = min(y0, y1), max(y0, y1)
-    return (ymin < r[0] < ymax) and (xmin < r[1] < xmax)
-
-
-class GUI(animation.TimedAnimation):
-    """
-        interface for viewing a movie and its associated roi, traces, other things
-
-        implementation is only through matplotlib, and is non-blocking
-        backend affects performance somewhat dramatically. have achieved decent performance with qt5agg and tkagg
-
-         Methods:
-         -------
-
-
-        Attributes:
-        ----------
-            roi_kept : boolean array of length of supplied roi, indicating whether or not roi should be kept based on user input
-
-
-    """
-    #todo: todocument *14
-
-    def __init__(self, mov, roi, traces, images={}, cmap=pl.cm.viridis, **kwargs):
-        """
-            Args:
-                mov : 3d np array, 0'th axis is time/frames
-
-                roi : 3d np array, one roi per item in 0'th axis, each of which is a True/False mask indicating roi (True=inside roi)
-
-                traces : 2d np array, 0'th axis is time, 1st axis is sources
-
-                images: dictionary of still images
-
-            Attributes:
-                roi_kept : boolean array of length of supplied roi, indicating whether or not roi should be kept based on user input
-
-        """
-
-        self.mov = mov
-        self.roi_idxs = np.array([np.argwhere(r.flat).squeeze() for r in roi])
-        self.roi_centers = np.array(
-            [np.mean(np.argwhere(r), axis=0) for r in roi])
-        self.roi_orig = roi.copy()
-        self.roi = pretty_roi(roi)
-        self.roi_kept = np.ones(len(self.roi_idxs)).astype(bool)
-        self.traces = traces
-        self.images = images
-
-        # figure setup
-        self.cmap = cmap
-        self.fig = pl.figure()
-        NR, NC = 128, 32
-        gs = gridspec.GridSpec(nrows=NR, ncols=NC)
-        gs.update(wspace=0.1, hspace=0.1, left=.04,
-                  right=.96, top=.98, bottom=.02)
-
-        # movie axes
-        self.ax_contrast0 = self.fig.add_subplot(gs[0:5, 0:NC // 3])
-        self.ax_contrast1 = self.fig.add_subplot(gs[5:10, 0:NC // 3])
-        self.ax_mov = self.fig.add_subplot(gs[10:55, 0:NC // 3])
-        self.ax_mov.axis('off')
-        self.ax_img = self.fig.add_subplot(gs[55:100, 0:NC // 3])
-        self.ax_img.axis('off')
-        self.axs_imbuts = [self.fig.add_subplot(
-            gs[110:128, idx * 2:idx * 2 + 2]) for idx, i in enumerate(self.images)]
-
-        # trace axes
-        self.ax_trcs = self.fig.add_subplot(gs[0:64, NC // 2:])
-        self.ax_trc = self.fig.add_subplot(gs[65:85, NC // 2:])
-        self.ax_trc.set_xlim([0, len(self.traces)])
-        self.ax_nav = self.fig.add_subplot(gs[85:90, NC // 2:])
-        self.ax_nav.set_xlim([0, len(self.traces)])
-        self.ax_nav.axis('off')
-        self.ax_rm = self.fig.add_subplot(gs[95:110, NC // 2:])
-
-        # interactivity
-        self.c0, self.c1 = 0, 100
-        self.sl_contrast0 = Slider(
-            self.ax_contrast0, 'Low', 0., 255.0, valinit=self.c0, valfmt='%d')
-        self.sl_contrast1 = Slider(
-            self.ax_contrast1, 'Hi', 0., 255.0, valinit=self.c1, valfmt='%d')
-        self.sl_contrast0.on_changed(self.evt_contrast)
-        self.sl_contrast1.on_changed(self.evt_contrast)
-        self.img_buttons = [Button(ax, k) for k, ax in zip(
-            list(self.images.keys()), self.axs_imbuts)]
-        self.but_rm = Button(self.ax_rm, 'Remove All ROIs Currently in FOV')
-
-        # display initial things
-        self.movdata = self.ax_mov.imshow(self.mov[0])
-        self.movdata.set_animated(True)
-        self.roidata = self.ax_mov.imshow(
-            self.roi, cmap=self.cmap, alpha=0.5, vmin=np.nanmin(self.roi), vmax=np.nanmax(self.roi))
-        self.trdata, = self.ax_trc.plot(np.zeros(len(self.traces)))
-        self.navdata, = self.ax_nav.plot(
-            [-2, -2], [-1, np.max(self.traces)], 'r-')
-        if len(self.images):
-            lab, im = list(self.images.items())[0]
-            self.imgdata = self.ax_img.imshow(im)
-            self.ax_img.set_ylabel(lab)
-        self.plot_current_traces()
-
-        # callbacks
-        for ib, lab in zip(self.img_buttons, list(self.images.keys())):
-            ib.on_clicked(lambda evt, lab=lab: self.evt_imbut(evt, lab))
-        self.but_rm.on_clicked(self.remove_roi)
-        self.fig.canvas.mpl_connect('button_press_event', self.evt_click)
-        self.ax_mov.callbacks.connect('xlim_changed', self.evt_zoom)
-        self.ax_mov.callbacks.connect('ylim_changed', self.evt_zoom)
-
-        # runtime
-        self._idx = -1
-        self.t0 = time.clock()
-        self.always_draw = [self.movdata, self.roidata, self.navdata]
-        self.blit_clear_axes = [self.ax_mov, self.ax_nav]
-
-        # parent init
-        animation.TimedAnimation.__init__(
-            self, self.fig, interval=40, blit=True, **kwargs)
-
-    @property
-    def frame_seq(self):
-        self._idx += 1
-        if self._idx == len(self.mov):
-            self._idx = 0
-        self.navdata.set_xdata([self._idx, self._idx])
-        yield self.mov[self._idx]
-
-    @frame_seq.setter
-    def frame_seq(self, val):
-        pass
-
-    def new_frame_seq(self):
-        return self.mov
-
-    def _init_draw(self):
-        self._draw_frame(self.mov[0])
-        self._drawn_artists = self.always_draw
-
-    def _draw_frame(self, d):
-        self.t0 = time.clock()
-        self.movdata.set_data(d)
-        # blit
-        self._drawn_artists = self.always_draw
-        for da in self._drawn_artists:
-            da.set_animated(True)
-
-    def _blit_clear(self, artists, bg_cache):
-        for ax in self.blit_clear_axes:
-            if ax in bg_cache:
-                self.fig.canvas.restore_region(bg_cache[ax])
-
-    def evt_contrast(self, val):
-        self.c0 = self.sl_contrast0.val
-        self.c1 = self.sl_contrast1.val
-
-        if self.c0 > self.c1:
-            self.c0 = self.c1 - 1
-            self.sl_contrast0.set_val(self.c0)
-        if self.c1 < self.c0:
-            self.c1 = self.c0 + 1
-            self.sl_contrast1.set_val(self.c1)
-
-        self.movdata.set_clim(vmin=self.c0, vmax=self.c1)
-        self.imgdata.set_clim(vmin=self.c0, vmax=self.c1)
-
-    def evt_imbut(self, evt, lab):
-        self.imgdata.set_data(self.images[lab])
-        self.ax_img.set_title(lab)
-
-    def evt_click(self, evt):
-        if not evt.inaxes:
-            return
-
-        elif evt.inaxes == self.ax_mov:
-            # select roi
-            x, y = int(np.round(evt.xdata)), int(np.round(evt.ydata))
-            idx = np.ravel_multi_index((y, x), self.roi.shape)
-            inside = np.argwhere([idx in ri for ri in self.roi_idxs])
-            if len(inside) == 0:
-                return
-            i = inside[0]
-            self.set_current_trace(i)
-
-        elif evt.inaxes in [self.ax_nav]:
-            x = int(np.round(evt.xdata))
-            self._idx = x
-
-    def evt_zoom(self, *args):
-        self.plot_current_traces()
-
-    def set_current_trace(self, idx):
-        col = self.cmap(np.linspace(0, 1, np.sum(self.roi_kept)))[
-            np.squeeze(idx)]
-        t = self.traces[:, idx]
-        self.trdata.set_ydata(t)
-        self.trdata.set_color(col)
-        self.ax_trc.set_ylim([t.min(), t.max()])
-        self.ax_trc.set_title('ROI {}'.format(idx))
-        self.ax_trc.figure.canvas.draw()
-
-    def get_current_roi(self):
-        croi = np.array([isin(rc, self.ax_mov) for rc in self.roi_centers])
-        croi[self.roi_kept is False] = False
-        return croi
-
-    def remove_roi(self, evt):
-        self.current_roi = self.get_current_roi()
-        self.roi_kept[self.current_roi] = False
-
-        # update
-        if np.sum(self.roi_kept):
-            proi = pretty_roi(self.roi_orig[self.roi_kept])
-            self.roidata.set_data(proi)
-            self.roidata.set_clim(vmin=np.nanmin(proi), vmax=np.nanmax(proi))
-        else:
-            self.roidata.remove()
-
-    def plot_current_traces(self):
-        self.current_roi = self.get_current_roi()
-
-        if np.sum(self.current_roi) == 0:
-            return
-        for line in self.ax_trcs.get_lines():
-            line.remove()
-
-        cols = self.cmap(np.linspace(0, 1, len(self.roi_idxs)))[
-            self.current_roi]
-        lastmax = 0
-        for t, c in zip(self.traces.T[self.current_roi], cols):
-            self.ax_trcs.plot((t - t.min()) + lastmax, color=c)
-            lastmax = np.max(t)
-        self.ax_trcs.set_ylim([0, lastmax])
-
-
-#%%
-# FIXME below, load() and fname are not defined
-if __name__ == '__main__':
-    # todo: load and fname ??
-    # load data
-    my_mov = load(fname, fr=30)
-    mean, minn, maxx = my_mov.mean(axis=0), my_mov.min(axis=0), my_mov.max(axis=0)
-    my_roi = np.load('/Users/ben/Desktop/roi.npy') # TODO: Remove this entire function?
-    tr = np.random.random([3000, 3])
-    # run interface
-    intfc = GUI(my_mov, my_roi, tr, images=dict(mean=mean, min=minn, max=maxx))

caiman/gui/gui_pyqtgraph.py
@@ -1,3 +1,0 @@
-#%%
-import pyqtgraph.examples
-pyqtgraph.examples.run()

commit 6df5a5a84b241a18c50c458e1f67123461b314a1

reverting bug introduced with 48b644d30be4d063619511a160e8e422deeac0eb

caiman/source_extraction/cnmf/estimates.py
@@ -1310,9 +1310,12 @@ class Estimates(object):
         else:
             components_to_keep = np.arange(self.A.shape[-1])
 
-
-
-        self.select_components(idx_components=components_to_keep)
+        if self.idx_components is None:
+            self.idx_components = np.arange(self.A.shape[-1])
+        self.idx_components = np.intersect1d(self.idx_components, components_to_keep)
+        self.idx_components_bad = np.setdiff1d(np.arange(self.A.shape[-1]), self.idx_components)
+        if select_comp:
+            self.select_components(use_object=True)
 
         return components_to_keep
 